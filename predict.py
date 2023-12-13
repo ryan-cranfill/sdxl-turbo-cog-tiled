0d286891eb98aa536d50d2bcb22c1a7ef3be0328
Ryan Cranfill <ryanccranfill@gmail.com>
2023-12-13 17:42:42 -0600

    initial commit with working tiling

--- predict.py
+++ predict.py
@@ -8,14 +8,48 @@ from diffusers import AutoPipelineForText2Image
 MODEL_NAME = "stabilityai/sdxl-turbo"
 MODEL_CACHE = "model-cache"
 
+
 class Predictor(BasePredictor):
     def setup(self) -> None:
-        self.pipe = AutoPipelineForText2Image.from_pretrained(
+        pipe = AutoPipelineForText2Image.from_pretrained(
             MODEL_NAME,
             cache_dir=MODEL_CACHE,
             torch_dtype=torch.float16,
-            variant="fp16"     
-        ).to("cuda")
+            variant="fp16"
+        )
+        pipe.vae.use_tiling = False
+
+        from typing import Optional
+        import diffusers
+        from torch import Tensor
+        from torch.nn import functional as F
+        from torch.nn import Conv2d
+        from torch.nn.modules.utils import _pair
+
+        def asymmetricConv2DConvForward(self, input: Tensor, weight: Tensor, bias: Optional[Tensor]):
+            self.paddingX = (self._reversed_padding_repeated_twice[0], self._reversed_padding_repeated_twice[1], 0, 0)
+            self.paddingY = (0, 0, self._reversed_padding_repeated_twice[2], self._reversed_padding_repeated_twice[3])
+            working = F.pad(input, self.paddingX, mode='circular')
+            working = F.pad(working, self.paddingY, mode='circular')
+            return F.conv2d(working, weight, bias, self.stride, _pair(0), self.dilation, self.groups)
+
+        targets = [pipe.vae, pipe.text_encoder, pipe.unet, ]
+        conv_layers = []
+        for target in targets:
+            for module in target.modules():
+                if isinstance(module, torch.nn.Conv2d):
+                    conv_layers.append(module)
+
+        for cl in conv_layers:
+            if isinstance(cl, diffusers.models.lora.LoRACompatibleConv) and cl.lora_layer is None:
+                cl.lora_layer = lambda *x: 0
+
+            cl._conv_forward = asymmetricConv2DConvForward.__get__(cl, torch.nn.Conv2d)
+
+        if torch.cuda.is_available():
+            pipe.to("cuda")
+        self.pipe = pipe
+        self.pipe.vae.enable_tiling()
 
     def predict(
         self,
@@ -40,6 +74,22 @@ class Predictor(BasePredictor):
         seed: int = Input(
             description="Random seed. Leave blank to randomize the seed", default=None
         ),
+        width: int = Input(
+            description="Width of the output image",
+            ge=1, le=1024, default=512,
+        ),
+        height: int = Input(
+            description="Height of the output image",
+            ge=1, le=1024, default=512,
+        ),
+        final_width: int = Input(
+            description="Width of the final output image",
+            ge=1, le=1024, default=512,
+        ),
+        final_height: int = Input(
+            description="Height of the final output image",
+            ge=1, le=1024, default=512,
+        ),
     ) -> List[Path]:
         """Run a single prediction on the model"""
         if seed is None:
@@ -53,12 +103,16 @@ class Predictor(BasePredictor):
             "guidance_scale": 0,
             "generator": generator,
             "num_inference_steps": num_inference_steps,
+            "width": width,
+            "height": height,
         }
 
         output = self.pipe(**common_args)
 
         output_paths = []
         for i, image in enumerate(output.images):
+            if final_width != width or final_height != height:
+                image = image.resize((final_width, final_height))
             output_path = f"/tmp/out-{i}.png"
             image.save(output_path)
             output_paths.append(Path(output_path))
---
+++ test_tiling.py
@@ -0,0 +1,48 @@
+import time
+import requests
+import cv2
+import base64
+import numpy as np
+
+SERVER_URL = 'http://192.168.1.17:5001'
+
+input_dict = {
+    "input": {
+        "prompt": "colorful husky watercolor painting",
+        "num_outputs": 2,
+        "num_inference_steps": 4,
+        "width": 512,
+        "height": 512,
+        "final_width": 512,
+        "final_height": 512,
+    }
+}
+
+start = time.time()
+
+response = requests.post(SERVER_URL + '/predictions', json=input_dict)
+response_time = time.time() - start
+
+if response.ok:
+    # Output images are base64 encoded pngs in response.json()['output']
+    data = response.json()
+    for i, image in enumerate(data['output']):
+        tile_x = 4
+        tile_y = 3
+        # Remove the header and decode the image
+        image = image.split(',')[1]
+        image = base64.b64decode(image)
+        # Convert to numpy array
+        image = cv2.imdecode(np.frombuffer(image, np.uint8), -1)
+        # Tile the image
+        image = np.tile(image, (tile_x, tile_y, 1))
+        # Save the image
+        cv2.imwrite(f'output_{i}.png', image)
+
+end = time.time()
+print(f'Time taken: {end - start}')
+print(f'Response time: {response_time}')
+
+
+        # with open(f'output_{i}.png', 'wb') as f:
+        #     f.write(base64.b64decode(image))
